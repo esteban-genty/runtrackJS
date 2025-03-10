72fd09b9b4520b348e5802137bc31d4865109ffe
esteban-genty <esteban.genty@laplateforme.io>
2025-03-10 13:38:51 +0100

    Ajout du JOB05 du Jour01

---
+++ Jour01/JOB05/script.js
@@ -0,0 +1,12 @@
+function afficherJoursSemaines() {
+    // Tableau des jours
+    let joursSemaines = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+
+    // Boucle pour afficher les jours de la semaine
+    for (let i = 0; i < joursSemaines.length; i++) {
+        console.log(joursSemaines[i]); // Affiche chaque jour de la semaine
+    }
+}
+
+// Affichage des résultats
+afficherJoursSemaines();
